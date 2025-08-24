8c3b8d9432e522758afff2aae873b35a213a11a7
Darkzarich <darkzarichv2@gmail.com>
2025-08-25 02:42:59 +0300

    stage-2 make core service proxy dialog requests to dialog service via http-proxy-middleware

--- stage-2/core_service/controllers/list-messages.controller.js
+++
@@ -1,9 +0,0 @@
-import { connection as db } from '../db.js';
-
-export async function listMessages(req, res) {
-  const { id: currentUserId } = req.user;
-
-  const { user_id: targetUserId } = req.params;
-
-  // Send request to the dialog service
-}
--- stage-2/core_service/routes/index.route.js
+++ stage-2/core_service/routes/index.route.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { testRouter } from './test.route.js';
 import { userRouter } from './user.route.js';
-import { dialogRouter } from './dialog.route.js';
+import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
 
 const indexRouter = express.Router();
 
-indexRouter.use('/api/dialog', dialogRouter);
+indexRouter.use('/api/dialog', createProxyMiddleware({
+  target: process.env.DIALOG_SERVICE_URL + '/api/dialog',
+  on: {
+    proxyReq: fixRequestBody,
+  },
+}));
 indexRouter.use('/api/user', userRouter);
 indexRouter.use('/api/test', testRouter);
 
